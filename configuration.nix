73103c70c87deab218e321bc89438418a021c641
top-on <top-on@posteo.de>
2023-11-14 21:34:58 +0100

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices."luks-22bb5904-0453-478a-b2a0-2f9192b5c485".device = "/dev/disk/by-uuid/22bb5904-0453-478a-b2a0-2f9192b5c485";
+
+  # networking
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # location
+  time.timeZone = "Europe/Berlin";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "altgr-intl";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account.
+  users.users.thor = {
+    isNormalUser = true;
+    description = "thor";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      thunderbird
+      keepassxc
+      syncthing
+      brave
+      signal-desktop
+      element-desktop
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # system tools
+    git
+    htop
+    vim 
+    wget
+    mullvad-vpn
+    # gnome
+    gnomeExtensions.mullvad-indicator
+    gnome.gnome-tweaks
+  ];
+
+  # List services that you want to enable:
+  services = {
+  
+    syncthing = { 
+      enable = true;
+      user = "thor";
+      dataDir = "/home/thor/";
+    };
+    mullvad-vpn.enable=true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  system.stateVersion = "23.05"; 
+}
